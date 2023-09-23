6fe7ada08be4a12a4031c809def8db12b6967fda
raildsonf <rfelipe17@outlook.com>
2023-09-23 12:23:30 -0300

    generic commit

--- aws/codecommit-trigger/main.tf
+++ aws/codecommit-trigger/main.tf
@@ -2,7 +2,7 @@ resource "aws_codecommit_trigger" "main" {
   repository_name = var.repository_name
 
   trigger {
-    name            = var.trigger_name
+    name            = var.repository_name
     events          = var.trigger_name
     destination_arn = var.aws_sns_topic
   }
--- proj/main.tf
+++ proj/main.tf
@@ -1,11 +1,11 @@
 module "main" {
-  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit?ref=v1.0.8"
+  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit?ref=v1.0.9"
   repository_name = "teste"
   default_tags = var.default_tags
 }
 
 module "main_trigger" {
-  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit-trigger?ref=v1.0.8"
+  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit-trigger?ref=v1.0.9"
   repository_name = "teste"
   trigger_name = ["all"]
   aws_sns_topic = "arn:aws:sns:us-east-2:013281578681:CodeStarNotifications-devops-notifica-ea4c6312a0338d9601660a38310fe737aa54a9ee"
