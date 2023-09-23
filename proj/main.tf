f697ea6b9c6843095196203d39e8a0769857a728
raildsonf <rfelipe17@outlook.com>
2023-09-23 13:06:26 -0300

    generic commit

--- proj/main.tf
+++ proj/main.tf
@@ -1,11 +1,12 @@
 module "main" {
-  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit?ref=v1.0.10"
+  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit?ref=v1.0.12"
   repository_name = "teste"
+  default_branch = "master"
   default_tags = var.default_tags
 }
 
 module "main_trigger" {
-  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit-trigger?ref=v1.0.10"
+  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit-trigger?ref=v1.0.12"
   repository_name = "teste"
   trigger_name = ["all"]
   aws_sns_topic = "arn:aws:sns:us-east-2:013281578681:testes"
