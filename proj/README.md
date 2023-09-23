f4e0c3740380789e1aa0d2bdd96ab1806642c983
raildsonf <rfelipe17@outlook.com>
2023-09-22 22:47:26 -0300

    generic commit

---
+++ proj/README.md
@@ -0,0 +1,2 @@
+terraform plan -var-file=./default_tags/default_tags.tfvars
+terraform apply -var-file=./default_tags/default_tags.tfvars
--- proj/main.tf
+++ proj/main.tf
@@ -1,8 +1,5 @@
 module "main" {
   source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit?ref=v1.0.4"
   repository_name = "teste"
-  default_tags = {
-    Team          = "admin"
-    OU            = "SRE"
-  }
+  default_tags = var.default_tags
 }
