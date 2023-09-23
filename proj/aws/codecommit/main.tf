f0926e45af0e459bf2fc295924bc9d7ed68f613f
raildsonf <rfelipe17@outlook.com>
2023-09-23 14:19:24 -0300

    generic commit

--- proj/aws/codecommit/main.tf
+++ proj/aws/codecommit/main.tf
@@ -1,5 +1,5 @@
 module "main" {
-  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit?ref=v1.0.17"
+  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit?ref=v1.0.18"
   repository_name = "teste"
   default_tags = {
     PROJECT = "demo"
@@ -8,17 +8,15 @@ module "main" {
 }
 
 module "main_trigger" {
-  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit-trigger?ref=v1.0.17"
+  source          = "github.com/raildsonf/terraform-modules.git//aws/codecommit-trigger?ref=v1.0.18"
   repository_name = "teste"
   trigger_name    = ["all"]
   aws_sns_topic   = "arn:aws:sns:us-east-2:013281578681:testes"
-  depends_on      = [module.main.repo_name]
 }
 
 module "main_approvals" {
-  source                = "github.com/raildsonf/terraform-modules.git//aws/codecommit-approval-rule-template?ref=v1.0.17"
+  source                = "github.com/raildsonf/terraform-modules.git//aws/codecommit-approval-rule-template?ref=v1.0.18"
   repository_name       = "teste"
   number_approvals      = 1
   approval_pool_members = ["arn:aws:iam::013281578681:user/admin"]
-  depends_on            = [module.main.repo_name]
 }
