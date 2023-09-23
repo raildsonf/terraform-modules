3b5c5bf735f27c6321f9b4ff423496354b0cdc7b
raildsonf <rfelipe17@outlook.com>
2023-09-23 12:50:16 -0300

    generic commit

--- aws/codecommit/versions.tf
+++ aws/codecommit/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">=0.12"
+  required_version = ">=1.5.6"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.58"
+      version = "~> 4.0.0"
     }
   }
 }
