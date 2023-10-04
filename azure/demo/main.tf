b6439a6afa141b0045fa6d65a7084e5aa4a3d52c
raildsonf <rfelipe17@outlook.com>
2023-10-04 17:11:31 -0300

    generic commit

---
+++ azure/demo/main.tf
@@ -0,0 +1,26 @@
+module "resource_group_main" {
+  source              = "github.com/raildsonf/terraform-modules.git//azure/resource-group?ref=main"
+  resource_group_name = "main"
+  location            = "West US"
+  tags = {
+    TEAM    = "GK",
+    STACK   = "webtier",
+    OU      = "SRE",
+    PROJECT = "unit"
+  }
+}
+
+module "vnet_main" {
+  source              = "github.com/raildsonf/terraform-modules.git//azure/vnet?ref=main"
+  resource_group_name = "main"
+  location            = "West US"
+  vnet_name           = "aks"
+  address_space       = "10.0.0.0/16"
+  tags = {
+    TEAM    = "GK",
+    STACK   = "webtier",
+    OU      = "SRE",
+    PROJECT = "unit"
+  }
+  depends_on = [ module.resource_group_main ]
+}
